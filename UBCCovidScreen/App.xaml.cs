a05426704613ea9b82aa02f9179af94a671f8dca
henrylp <laprairieh@gmail.com>
2021-01-09 16:42:47 -0800

    Add project files.

---
+++ UBCCovidScreen/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UBCCovidScreen
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            InitializeComponent();
+
+
+            bool isLoggedIn = Current.Properties.ContainsKey("IsLoggedIn") ? Convert.ToBoolean(Current.Properties["IsLoggedIn"]) : false;
+            if (!isLoggedIn)
+            {
+                //Load if Not Logged In
+                MainPage = new NavigationPage(new MainPage());
+            }
+            else
+            {
+                MainPage = new NavigationPage(new HomePage());
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ UBCCovidScreen/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UBCCovidScreen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            bool isFNEmpty = string.IsNullOrEmpty(fName.Text);
+            bool isLNEmpty = string.IsNullOrEmpty(lName.Text);
+            bool isSportEmpty = string.IsNullOrEmpty(SportPicker.Text);
+            if(isFNEmpty|| isLNEmpty || isSportEmpty)
+            {
+                DisplayAlert("Error", "Please fill out all fields", "OK");
+            }
+            else
+            {
+                Application.Current.Properties["IsLoggedIn"] = Boolean.TrueString;
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
